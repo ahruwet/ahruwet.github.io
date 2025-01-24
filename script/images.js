81fc44ee8c62ae5a770506e15d93299f04f39d8d
AuSpace <85060725+ahruwet@users.noreply.github.com>
2025-01-24 23:38:34 +0700

    Add files via upload

---
+++ script/images.js
@@ -0,0 +1,22 @@
+function showImage(img) {
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modalImg");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+var modal = document.getElementById("modal");
+var modalImg = document.getElementById("modalImg");
+modal.addEventListener("click", function(event) {
+  if (event.target != modalImg) {
+    modal.style.display = "none";
+  }
+});
